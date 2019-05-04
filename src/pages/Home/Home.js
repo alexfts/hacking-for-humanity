ac312a4b14bf5fbd7a246f901d5889c7006861b8
nejmal <lam.jennifer.ky@gmail.com>
2019-05-04 16:45:14 -0700

    Move images to public folder.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import { Button, withStyles, Typography, Grid } from '@material-ui/core';
 import styles from './styles';
+// import classes from '*.module.css';
 
-const Home = props => (
-  <div>
-    Home page!
-    <Button aria-label="Select your trash" component={Link} to="/choose-litter">
-      Start
-    </Button>
-  </div>
+const Home = ({ classes }) => (
+  <Grid
+    container
+    className={classes.container}
+    direction="row"
+    alignItems="center"
+    justify="center"
+  >
+    <Grid item xs={12} sm={12} md={6} className={classes.item}>
+      <Typography variant="display4" className={classes.headline}>
+        Reduce. Reuse. Recycle.
+      </Typography>
+    </Grid>
+    <Grid item xs={12} sm={12} md={6} className={classes.item}>
+      <Button
+        variant="outlined"
+        aria-label="Select your trash"
+        component={Link}
+        to="/choose-litter"
+        color="secondary"
+      >
+        Start
+      </Button>
+    </Grid>
+  </Grid>
 );
 
 export default withStyles(styles)(Home);
