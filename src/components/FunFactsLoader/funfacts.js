6cdbde92931e9a69ca362c6adaef6e2ae69ebad6
Jenna Pegg <jenna.pegg.vic@gmail.com>
2019-05-04 17:01:49 -0700

    add FunFacts in componetents and pull from funfacts.json

---
+++ src/components/FunFactsLoader/funfacts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+import funFacts from '../../data/funfacts.json';
+
+const FunFacts = props => {
+    var Random = funFacts.facts[Math.floor(Math.random() * 13)];
+    
+    return (
+    <div>
+        {Random}
+    </div>
+);
+    }
+export default withStyles(styles)(FunFacts);
