54fc6585364097f59e689254bf63507ce0a3892f
alexfts <alxfetisova@gmail.com>
2019-05-04 18:17:30 -0700

    implement unlocking

--- src/pages/Compost/Compost.js
+++ src/pages/Compost/Compost.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import litterData from '../../data/litter.json';
 import styles from './styles';
+
+const unlockNewItem = () => {
+  const unlockedItems = localStorage.getItem('unlockedItems').split(',');
+  const newItemName = Object.keys(litterData.litter)[unlockedItems.length];
+  localStorage.setItem('unlockedItems', [...unlockedItems, newItemName]);
+};
 
 const Compost = ({ location }) => {
   if (localStorage.getItem('unlockedItems') === null) {
@@ -10,6 +17,18 @@ const Compost = ({ location }) => {
   }
   const litter = location.state && location.state.litter;
   const name = location.state && location.state.name;
+  const isCorrectMethod = litter && litter.correctMethod === 'compost';
+  if (isCorrectMethod) {
+    // ensure we haven't visited this item yet
+    const visitedItems = localStorage.getItem('visitedItems');
+    if (!visitedItems) {
+      unlockNewItem();
+      localStorage.setItem('visitedItems', [name]);
+    } else if (!visitedItems.includes(name)) {
+      unlockNewItem();
+      localStorage.setItem('visitedItems', [...visitedItems.split(','), name]);
+    }
+  }
   return (
     <div>
       Compost
--- src/pages/Landfill/Landfill.js
+++ src/pages/Landfill/Landfill.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import litterData from '../../data/litter.json';
 import styles from './styles';
+
+const unlockNewItem = () => {
+  const unlockedItems = localStorage.getItem('unlockedItems').split(',');
+  const newItemName = Object.keys(litterData.litter)[unlockedItems.length];
+  localStorage.setItem('unlockedItems', [...unlockedItems, newItemName]);
+};
 
 const Landfill = ({ location }) => {
   if (localStorage.getItem('unlockedItems') === null) {
@@ -10,6 +17,18 @@ const Landfill = ({ location }) => {
   }
   const litter = location.state && location.state.litter;
   const name = location.state && location.state.name;
+  const isCorrectMethod = litter && litter.correctMethod === 'landfill';
+  if (isCorrectMethod) {
+    // ensure we haven't visited this item yet
+    const visitedItems = localStorage.getItem('visitedItems');
+    if (!visitedItems) {
+      unlockNewItem();
+      localStorage.setItem('visitedItems', [name]);
+    } else if (!visitedItems.includes(name)) {
+      unlockNewItem();
+      localStorage.setItem('visitedItems', [...visitedItems.split(','), name]);
+    }
+  }
   return (
     <div>
       Landfill
